c0de87eaecbe45155fe44a85e516bb15794ed954
Aloshya-QA <alexis.belyakoff@yandex.ru>
2025-05-21 00:35:14 +0300

    Add test annotations, testng xml with cross-browsing and Retry + Listener

--- src/test/java/tests/NavigationModuleTest.java
+++ src/test/java/tests/NavigationModuleTest.java
@@ -1,6 +1,5 @@
 package tests;
 
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
